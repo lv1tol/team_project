80781b0924e9e34715fa9140a3e3f9056734031a
andrii9530 <144731257+andrii9530@users.noreply.github.com>
2025-02-01 20:03:57 +0200

    Update models.py

--- products/models.py
+++ products/models.py
@@ -5,3 +5,4 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)
