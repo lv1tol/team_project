ad2fefd3799f068e02fe7d034a25142267fcd906
Victor <vityasafronuk30@gmail.com>
2025-02-05 22:44:28 +0200

    CrUd

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from products import views
+
 
 urlpatterns = [
+    path('', views.product_list),
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),
+    path('edit/<int:id>', views.edit_product),
+    path('delete/<int:id>', views.delete_product),
+    path('add', views.add_product),
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Product
 from .forms import ProductForm
+import os
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Список товарів
 def product_list(request):
@@ -8,12 +11,33 @@ def product_list(request):
     return render(request, 'products/product_list.html', {'products': products})
 
 # Додавання товару
+
+
 def add_product(request):
+    form = ProductForm()
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('product_list')
+            return redirect("/")
+    return render(request, "products/add_product.html", {"form": form, "id": id})
+
+# Оновлення товару 
+def edit_product(request, id):
+    product = Product.objects.get(id=id)
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
     else:
-        form = ProductForm()
-    return render(request, 'products/add_product.html', {'form': form})
+        form = ProductForm(instance=product)
+    return render(request, 'products/edit_product.html', {'form': form})
+
+# Видалення товару
+def delete_product(request, id):
+    product = Product.objects.get(pk=id)
+    if product is None:
+        return HttpResponse("Guitar not found")
+    product.delete()
+    return redirect("/")
