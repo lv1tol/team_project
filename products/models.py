a74d813fa55d5027d3a549f93d73285c0afd4f6c
Victor <vityasafronuk30@gmail.com>
2025-02-18 18:19:45 +0200

    added wishlist & and fixed -dig

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User  
+from django.core.validators import MinValueValidator
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -20,11 +21,12 @@ class Product(models.Model):
     ]
 
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
     description = models.TextField()
     category = models.CharField(max_length=40, choices=CATEGORY_CHOICES)  
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    favorites = models.ManyToManyField(User, related_name='favorite_products', blank=True)
 
     def str(self):
         return f"{self.name} - {self.author.username}"
--- products/urls.py
+++ products/urls.py
@@ -12,6 +12,8 @@ urlpatterns = [
     path('delete/<int:id>/', delete_product, name='delete_product'),
     path("details/<int:id>", details, name='details'),
     path('my_listings', my_listings, name='my_listings'),
+    path('add_to_favorites/<int:id>', add_to_favorites, name='add_to_favorites'),
+    path('my_favorites', my_favorites, name='my_favorites'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html', next_page='product_list'), name='login'),
     path('register/', register, name='register'),
     path('logout/', logout_view, name='logout'),
