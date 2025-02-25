35537399da825cc4f2c84f828fd82971fc8fe508
Gavri_gu02@student.itstep.org <Gavri_gu02@student.itstep.org>
2025-02-25 19:42:26 +0200

    Кошик і покупка

--- products/models.py
+++ products/models.py
@@ -37,3 +37,10 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.user.username}"
